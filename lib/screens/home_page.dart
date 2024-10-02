3518a5fc5d25be86d2a6405cbd8ef8520aa6e8e6
PyPiSan <sanjeev.mishra96@gmail.com>
2024-10-02 23:12:17 +0530

    nav bar changed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
 // import 'package:minimal_flutter_app/screens/chat_page.dart';
 
-class HomePage extends StatelessWidget {
-  int currentPageIndex = 0;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Anime',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Drama',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Live',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // old Navigation bar
@@ -31,115 +66,149 @@ class HomePage extends StatelessWidget {
     //     )
     //     );
 
-    final ThemeData theme = Theme.of(context);
+    // Different Version of Bottom Navigation
+    // final ThemeData theme = Theme.of(context);
+    // return Scaffold(
+    //   bottomNavigationBar: NavigationBar(
+    //     // onDestinationSelected: (int index) {
+    //     //   setState(() {
+    //     //     currentPageIndex = index;
+    //     //   });
+    //     // },
+    //     indicatorColor: Colors.amber,
+    //     selectedIndex: currentPageIndex,
+    //     destinations: const <Widget>[
+    //       NavigationDestination(
+    //         selectedIcon: Icon(Icons.home),
+    //         icon: Icon(Icons.home_outlined),
+    //         label: 'Home',
+    //       ),
+    //       NavigationDestination(
+    //         icon: Badge(child: Icon(Icons.notifications_sharp)),
+    //         label: 'Notifications',
+    //       ),
+    //       NavigationDestination(
+    //         icon: Badge(
+    //           label: Text('2'),
+    //           child: Icon(Icons.messenger_sharp),
+    //         ),
+    //         label: 'Messages',
+    //       ),
+    //     ],
+    //   ),
+    //   body: <Widget>[
+    //     /// Home page
+    //     Card(
+    //       shadowColor: Colors.transparent,
+    //       margin: const EdgeInsets.all(8.0),
+    //       child: SizedBox.expand(
+    //         child: Center(
+    //           child: Text(
+    //             'Home page',
+    //             style: theme.textTheme.titleLarge,
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+
+    //     /// Notifications page
+    //     const Padding(
+    //       padding: EdgeInsets.all(8.0),
+    //       child: Column(
+    //         children: <Widget>[
+    //           Card(
+    //             child: ListTile(
+    //               leading: Icon(Icons.notifications_sharp),
+    //               title: Text('Notification 1'),
+    //               subtitle: Text('This is a notification'),
+    //             ),
+    //           ),
+    //           Card(
+    //             child: ListTile(
+    //               leading: Icon(Icons.notifications_sharp),
+    //               title: Text('Notification 2'),
+    //               subtitle: Text('This is a notification'),
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+
+    //     /// Messages page
+    //     ListView.builder(
+    //       reverse: true,
+    //       itemCount: 2,
+    //       itemBuilder: (BuildContext context, int index) {
+    //         if (index == 0) {
+    //           return Align(
+    //             alignment: Alignment.centerRight,
+    //             child: Container(
+    //               margin: const EdgeInsets.all(8.0),
+    //               padding: const EdgeInsets.all(8.0),
+    //               decoration: BoxDecoration(
+    //                 color: theme.colorScheme.primary,
+    //                 borderRadius: BorderRadius.circular(8.0),
+    //               ),
+    //               child: Text(
+    //                 'Hello',
+    //                 style: theme.textTheme.bodyLarge!
+    //                     .copyWith(color: theme.colorScheme.onPrimary),
+    //               ),
+    //             ),
+    //           );
+    //         }
+    //         return Align(
+    //           alignment: Alignment.centerLeft,
+    //           child: Container(
+    //             margin: const EdgeInsets.all(8.0),
+    //             padding: const EdgeInsets.all(8.0),
+    //             decoration: BoxDecoration(
+    //               color: theme.colorScheme.primary,
+    //               borderRadius: BorderRadius.circular(8.0),
+    //             ),
+    //             child: Text(
+    //               'Hi!',
+    //               style: theme.textTheme.bodyLarge!
+    //                   .copyWith(color: theme.colorScheme.onPrimary),
+    //             ),
+    //           ),
+    //         );
+    //       },
+    //     ),
+    //   ][currentPageIndex],
+    // );
+
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        // onDestinationSelected: (int index) {
-        //   setState(() {
-        //     currentPageIndex = index;
-        //   });
-        // },
-        indicatorColor: Colors.amber,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Badge(child: Icon(Icons.notifications_sharp)),
-            label: 'Notifications',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              label: Text('2'),
-              child: Icon(Icons.messenger_sharp),
+        appBar: AppBar(),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.red,
             ),
-            label: 'Messages',
-          ),
-        ],
-      ),
-      body: <Widget>[
-        /// Home page
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-                style: theme.textTheme.titleLarge,
-              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Anime',
+              backgroundColor: Colors.green,
             ),
-          ),
-        ),
-
-        /// Notifications page
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 1'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 2'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-            ],
-          ),
-        ),
-
-        /// Messages page
-        ListView.builder(
-          reverse: true,
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  margin: const EdgeInsets.all(8.0),
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    'Hello',
-                    style: theme.textTheme.bodyLarge!
-                        .copyWith(color: theme.colorScheme.onPrimary),
-                  ),
-                ),
-              );
-            }
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Hi!',
-                  style: theme.textTheme.bodyLarge!
-                      .copyWith(color: theme.colorScheme.onPrimary),
-                ),
-              ),
-            );
-          },
-        ),
-      ][currentPageIndex],
-    );
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'Drama',
+              backgroundColor: Colors.purple,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Live',
+              backgroundColor: Colors.pink,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+        ));
   }
 }
