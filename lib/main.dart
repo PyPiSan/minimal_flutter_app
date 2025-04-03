82afb878ab726a66a0c91e42cce8865dff67181a
pypisan <sanjeev@pypisan.com>
2025-04-04 02:52:34 +0530

    mac os build added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:minimal_flutter_app/plugins/mac_plugins.dart'
+    if (dart.library.html) 'package:minimal_flutter_app/plugins/web_plugins.dart';
 import 'package:minimal_flutter_app/app.dart';
 
 Future<void> main() async {
   // Ensure that the widgets are initialized
   WidgetsFlutterBinding.ensureInitialized();
-  // Use Path based Strategy
-  usePathUrlStrategy();
+  // Use different plugins
+  configureApp();
   runApp(const App());
 }
