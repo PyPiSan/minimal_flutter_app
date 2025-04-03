3f0d023e862aa99128a890dabfa430162ffcf856
pypisan <sanjeev@pypisan.com>
2025-04-04 01:02:47 +0530

    logic template added

---
+++ lib/commons/widgets/layouts/templates/login_template.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_flutter_app/commons/styles/spacing_styles.dart';
+import 'package:minimal_flutter_app/utils/constants/colors.dart';
+import 'package:minimal_flutter_app/utils/constants/sizes.dart';
+
+class LoginTemplate extends StatelessWidget {
+  const LoginTemplate({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 550,
+        child: SingleChildScrollView(
+          child: Container(
+              padding: SpacingStyle.paddingWithAppBarHeight,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppSizes.cardRadiusLg),
+                  color: AppColors.white),
+              child: child),
+        ),
+      ),
+    );
+  }
+}
--- lib/ecommerce_ui/features/authentication/screens/responsive_screens/login_desktop_tablet.dart
+++ lib/ecommerce_ui/features/authentication/screens/responsive_screens/login_desktop_tablet.dart
@@ -1,35 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:minimal_flutter_app/commons/styles/spacing_styles.dart';
+import 'package:minimal_flutter_app/commons/widgets/layouts/templates/login_template.dart';
 import 'package:minimal_flutter_app/ecommerce_ui/features/authentication/screens/widgets/login_form.dart';
 import 'package:minimal_flutter_app/ecommerce_ui/features/authentication/screens/widgets/login_header.dart';
-import 'package:minimal_flutter_app/utils/constants/colors.dart';
-import 'package:minimal_flutter_app/utils/constants/sizes.dart';
 
 class LoginScreenDesktopTablet extends StatelessWidget {
   const LoginScreenDesktopTablet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: 550,
-        child: SingleChildScrollView(
-          child: Container(
-            padding: SpacingStyle.paddingWithAppBarHeight,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(AppSizes.cardRadiusLg),
-                color: AppColors.white),
-            child: const Column(
-              // Header
-              children: [
-                LoginHeader(),
-                // Form
-                LoginForm()
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return const LoginTemplate(
+        child: Column(
+      children: [
+        LoginHeader(),
+        // Form
+        LoginForm()
+      ],
+    ));
   }
 }
