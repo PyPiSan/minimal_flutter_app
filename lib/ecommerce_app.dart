654ea8475d86cf54867de7ece0123ff2a3264ece
pypisan <sanjeev@pypisan.com>
2025-04-04 23:29:47 +0530

    e-commerce app added

--- lib/ecommerce_app.dart
+++ lib/ecommerce_app.dart
@@ -1,42 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:minimal_flutter_app/commons/widgets/layouts/templates/site_layout.dart';
+import 'package:get/route_manager.dart';
+import 'package:minimal_flutter_app/ecommerce_ui/routes/app_route.dart';
+import 'package:minimal_flutter_app/ecommerce_ui/routes/route.dart';
+import 'package:minimal_flutter_app/utils/theme/theme.dart';
 
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const SiteTemplate(
-      desktop: Desktop(),
-      tablet: Tablet(),
-      mobile: Mobile(),
+    return GetMaterialApp(
+      title: 'Minimal Flutter Ecommerce App',
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.light,
+      darkTheme: AppTheme.darkTheme,
+      // debug mode switch
+      debugShowCheckedModeBanner: false,
+      getPages: AppRoute.pages,
+      initialRoute: Routes.login,
+      unknownRoute: GetPage(
+        name: '/page-not-found',
+        page: () => const Scaffold(
+          body: Center(
+            child: Text("Route Not Found"),
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text("Admin Panel"),
+        ),
+      ),
     );
-  }
-}
-
-class Desktop extends StatelessWidget {
-  const Desktop({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-class Tablet extends StatelessWidget {
-  const Tablet({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-class Mobile extends StatelessWidget {
-  const Mobile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
