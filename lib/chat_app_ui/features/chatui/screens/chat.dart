9acc903841cdd347b6ee945564aacbd9c3c7f79b
pypisan <sanjeev@pypisan.com>
2025-04-19 18:52:52 +0530

    chat tablet added

--- lib/chat_app_ui/features/chatui/screens/chat.dart
+++ lib/chat_app_ui/features/chatui/screens/chat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_flutter_app/chat_app_ui/features/chatui/screens/responsive_screens/chat_screen_desktop.dart';
+import 'package:minimal_flutter_app/chat_app_ui/features/chatui/screens/responsive_screens/chat_screen_tablet.dart';
 import 'package:minimal_flutter_app/commons/widgets/layouts/headers/minimal_headers.dart';
 import 'package:minimal_flutter_app/commons/widgets/layouts/navbar/collapsing_navbar_drawer.dart';
 import 'package:minimal_flutter_app/commons/widgets/layouts/templates/site_layout.dart';
@@ -10,11 +11,11 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SiteTemplate(
-        useLayout: true,
-        desktop: ChatScreenDesktop(),
-        sidebar: CollapsingNavbarDrawer(),
-        appHeader: const MinimalAppHeaders()
-        // mobile: LoginScreenMobile(),
-        );
+      useLayout: true,
+      desktop: ChatScreenDesktop(),
+      sidebar: CollapsingNavbarDrawer(),
+      appHeader: const MinimalAppHeaders(),
+      tablet: const ChatScreenTablet(),
+    );
   }
 }
---
+++ lib/chat_app_ui/features/chatui/screens/responsive_screens/chat_screen_tablet.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChatScreenTablet extends StatelessWidget {
+  const ChatScreenTablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('hello world')),
+    );
+  }
+}
